7eea0a50ffaec207f969e1a9862e5f082eb20b37
tai2 <hello@tai2.net>
2022-03-26 01:31:49 +0900

    トップページ更新(ボイサン等追加)

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ['./index.html'],
+    theme: {
+        extend: {
+            fontFamily: {
+                serif: ['"Sawarabi Mincho"', 'serif'],
+                sans: ['"M PLUS 1p"', 'sans-serif'],
+            }
+        },
+    },
+    plugins: [],
+}
